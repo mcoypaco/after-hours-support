cf70aeed487e7b7b05ebfed80962ff187cb3f51e
Marco Paco <marcopaco1230@gmail.com>
2017-08-10 02:48:41 +0800

    API service

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class ApiService {
+  headers: HttpHeaders = new HttpHeaders().set('Authorization', `Bearer ${localStorage.access_token}`)
+
+  constructor(private http: HttpClient) { }
+
+  get(url: string): Observable<any>  {
+    return this.http.get(`${environment.apiUrl}/api/${url}`, {
+      headers: this.headers
+    });
+  }
+
+  post(url: string, body?:object): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/api/${url}`, body, {
+      headers: this.headers
+    });
+  }
+
+  put(url: string, body?:object): Observable<any> {
+    return this.http.put(`${environment.apiUrl}/api/${url}`, body, {
+      headers: this.headers
+    });
+  }
+
+  delete(url): Observable<any> {
+    return this.http.delete(`${environment.apiUrl}/api/${url}`, {
+      headers: this.headers
+    });
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
 import { MomentModule } from 'angular2-moment';
 
 import { ErrorComponent } from './error/error.component';
+import { ApiService } from './api.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule,
+    HttpClientModule,
     MomentModule
   ],
   exports: [
     CommonModule,
     FlexLayoutModule,
+    HttpClientModule,
     MomentModule,
     ErrorComponent
   ],
-  declarations: [ErrorComponent]
+  declarations: [ErrorComponent],
+  providers: [ApiService]
 })
 export class SharedModule { }
