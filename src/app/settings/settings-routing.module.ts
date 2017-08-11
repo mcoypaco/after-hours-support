2fac5c1691f1110669680c16229cdf5f3f17f975
Marco Paco <marcopaco1230@gmail.com>
2017-08-12 03:58:34 +0800

    Removed auth guard. Parent already implements it

--- src/app/settings/settings-routing.module.ts
+++ src/app/settings/settings-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthGuard } from '../auth/auth.guard';
 import { AgentsComponent } from './agents/agents.component';
 import { CampaignsComponent } from './campaigns/campaigns.component';
 import { SettingsComponent } from './settings.component';
@@ -12,12 +11,10 @@ const routes: Routes = [
   { 
     path: 'agents',
     component: SettingsComponent,
-    canActivate: [AuthGuard],
     children: [
       {
         path: '',
         component: AgentsComponent,
-        canActivateChild: [AuthGuard],
       }
     ]
   },
