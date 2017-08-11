f439ecf859a57717959c0deacf6d4754fa6ba360
Marco Paco <marcopaco1230@gmail.com>
2017-08-12 03:56:07 +0800

    Redirect user when catching a 401 error

--- src/app/core/exception.service.ts
+++ src/app/core/exception.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-import { MdDialog } from '@angular/material';
+import { MdDialog, MdDialogRef } from '@angular/material';
 import { Router } from '@angular/router';
 
+import { AuthService } from '../auth/auth.service';
 import { Exception } from './exception';
 import { ExceptionDialogComponent } from './exception-dialog/exception-dialog.component';
 
@@ -12,28 +13,33 @@ export class ExceptionService implements Exception {
   message: string;
   statusText: string;
 
-  constructor(private dialog: MdDialog, private router: Router) { }
+  constructor(private dialog: MdDialog, private router: Router, private auth: AuthService) { }
 
   takeAction(error: HttpErrorResponse) : void {
     this.statusText = error.statusText;
-
     if(error.status === 401) this.unauthenticated(error);
     else if(error.status === 403) this.unauthorized(error);
-    else this.internalServerError(error);
+    else if(error.status === 422) this.unprocessable(error);
+    else if(error.status === 500) this.internalServerError(error);
+  }
+
+  redirectToLoginPage(){
+    this.auth.redirectUrl = this.router.url;
+    this.router.navigate(['/login']); 
   }
 
   unauthenticated(error: HttpErrorResponse) {
     this.message = 'Please login to continue.';
-
-    this.openDialog(this.redirectToLoginPage);
-  }
-
-  redirectToLoginPage(){
-    this.router.navigate(['']);
+    this.openDialog().afterClosed().subscribe(result => this.redirectToLoginPage());
   }
 
   unauthorized(error: HttpErrorResponse) {
     this.message = 'This action is unauthorized.';
+    this.openDialog();
+  }
+
+  unprocessable(error: HttpErrorResponse) {
+    this.message = 'Please check the form for errors.';
     this.openDialog();
   }
 
@@ -42,15 +48,13 @@ export class ExceptionService implements Exception {
     this.openDialog();
   }
 
-  openDialog(resolveError? : VoidFunction) {
-    let dialogRef = this.dialog.open(ExceptionDialogComponent, {
+  openDialog() : MdDialogRef<ExceptionDialogComponent> {
+    return this.dialog.open(ExceptionDialogComponent, {
       data: {
         statusText: this.statusText,
         message: this.message,
         buttonLabel: this.buttonLabel,
       }
     });
-
-    if(resolveError) dialogRef.afterClosed().subscribe(() => resolveError());
   }
 }
