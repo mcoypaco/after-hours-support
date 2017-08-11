897c3b87290e4a9e0353d573e07d0da01c713b2e
Marco Paco <marcopaco1230@gmail.com>
2017-08-12 03:55:21 +0800

    Added singleton services to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { MdButtonModule, MdDialogModule } from '@angular/material';
+import { MdButtonModule, MdDialogModule, MdSnackBarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from './../shared/shared.module';
@@ -8,17 +9,21 @@ import { FormValidationMessageService } from './form-validation-message.service'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ExceptionService } from './exception.service';
 import { ExceptionDialogComponent } from './exception-dialog/exception-dialog.component';
+import { ApiService } from './api.service';
+import { PushNotificationService } from './push-notification.service';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+    HttpClientModule,
     MdButtonModule,
     MdDialogModule,
+    MdSnackBarModule,
     NoopAnimationsModule,
     SharedModule,
   ],
   declarations: [PageNotFoundComponent, ExceptionDialogComponent],
   entryComponents: [ExceptionDialogComponent],
-  providers: [FormValidationMessageService, ExceptionService],
+  providers: [FormValidationMessageService, ExceptionService, ApiService, PushNotificationService],
 })
 export class CoreModule { }
